c75c61a7f80c6f4784f3ed8ddef0a9a574c4dd8e
priyanka <priyankanikam1798@gmail.com>
2020-10-25 00:46:10 +0530

    commiting pagelayout

---
+++ pagelayout.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("header").css({"height":"100px","width":"1200px","background-color":"grey","border":"1px solid black"});
+    $("h1").css({"color":"white","text-align":"center","padding":"20px 400px 20px 400px"});
+    $("h1").html("Cybage");
+
+    $("#aside1").css({"height":"400px","width":"300px","border":"1px solid black","background-color":"grey","float":"left"});
+    $("#aside1").html("Aside 1");
+
+    $("section").css({"height":"400px","width":"600px","border":"1px solid black","background-color":"grey","float":"left"});
+    $("section").html("section");
+
+    $("#aside2").css({"height":"400px","width":"295px","border":"1px solid black","background-color":"grey","float":"left"});
+    $("#aside2").html("Aside 2");
+
+    $("footer").css({"height":"100px","width":"1200px","background-color":"grey","border":"1px solid black","clear":"both"});
+    $("h3").html("copyrights@cybage.com");
+    $("h3").css({"color":"white","padding":"20px 400px 20px 400px","text-align":"center"});
+});
